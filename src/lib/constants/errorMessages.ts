c7bb3ccf04793bbae93911d456a68025a13ecd9e
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-08-13 23:07:08 -0700

    create file of error messages

---
+++ src/lib/constants/errorMessages.ts
@@ -0,0 +1,5 @@
+export const noAuthMsg = "You must be logged in to perform this action.";
+
+export const unauthMsg = "You are not allowed to access this application.";
+
+export const notAdminMsg = "You must be an admin to perform this action.";
