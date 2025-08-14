d04224aeee7aa70520ed07ad342d1c2142957bd1
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-08-13 23:08:28 -0700

    add role index for users table + create role map

--- src/lib/convex/schema.ts
+++ src/lib/convex/schema.ts
@@ -1,8 +1,13 @@
 import { authTables } from "@convex-dev/auth/server";
-import { defineSchema } from "convex/server";
+import { defineSchema, defineTable } from "convex/server";
+import { hierarchyValidator, userValidator } from "./validators";
 
 const schema = defineSchema({
   ...authTables,
+  users: defineTable(userValidator)
+    .index("email", ["email"])
+    .index("by_role", ["role"]),
+  hierarchy: defineTable(hierarchyValidator),
 });
 
 export default schema;
