190388a16afd2f69faca32100533959840f5f1a6
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-08-17 20:51:13 -0700

    add errors

--- src/lib/constants/errorMessages.ts
+++ src/lib/constants/errorMessages.ts
@@ -3,3 +3,6 @@ export const noAuthMsg = "You must be logged in to perform this action.";
 export const unauthMsg = "You are not allowed to access this application.";
 
 export const notDirectorMsg = "You must be a director to perform this action.";
+export const notJudgeMsg = "You must be a judge to perform this action.";
+
+export const genericErrMsg = "An error occurred. Please try again.";
