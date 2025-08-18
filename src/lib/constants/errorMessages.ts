caf76aac431c9098a510d5acee8cd2389acee66a
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-08-17 18:25:50 -0700

    modify noAdminMsg

--- src/lib/constants/errorMessages.ts
+++ src/lib/constants/errorMessages.ts
@@ -2,4 +2,4 @@ export const noAuthMsg = "You must be logged in to perform this action.";
 
 export const unauthMsg = "You are not allowed to access this application.";
 
-export const notAdminMsg = "You must be an admin to perform this action.";
+export const notDirectorMsg = "You must be a director to perform this action.";
