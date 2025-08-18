e88cebdd1a6bdb1b9017faca1d1cb309ad6559b9
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-08-17 20:52:59 -0700

    create criteria-related types for easy maintain

--- src/lib/types/judging.d.ts
+++ src/lib/types/judging.d.ts
@@ -1,6 +1,10 @@
 import type { Infer } from "convex/values";
+import { criteria } from "../constants/judging";
 import { projectValidator, scoreValidator } from "../convex/validators";
 
 export type Score = Infer<typeof scoreValidator>;
 
 export type Project = Infer<typeof projectValidator>;
+
+export type Criterions = (typeof criteria)[number];
+export type Criteria = Record<Criterions, number>;
