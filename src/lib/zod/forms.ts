18102d612ace821b857889cf47a1fc2f4fda5811
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-08-21 22:07:06 -0700

    refine form error msgs

--- src/lib/zod/forms.ts
+++ src/lib/zod/forms.ts
@@ -1,7 +1,10 @@
 import { z } from "zod";
 import { criteria } from "../constants/judging";
 
-const criteriaValue = z.number().min(1).max(5);
+const criteriaValue = z
+  .number()
+  .min(1, "Please move the slider to give your rating.")
+  .max(5, "Please do not rate this criterion above 5.");
 
 export const scoreFormSchema = z.record(z.enum(criteria), criteriaValue);
 export type scoreFormSchemaType = z.infer<typeof scoreFormSchema>;
