a9190a332a72a8a0fa32c68514353f8ede2d3c93
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-08-31 21:26:37 -0700

    remove mentor-specific err msg

--- src/lib/constants/errorMessages.ts
+++ src/lib/constants/errorMessages.ts
@@ -4,6 +4,5 @@ export const unauthMsg = "You are not allowed to access this application.";
 
 export const notDirectorMsg = "You must be a director to perform this action.";
 export const notJudgeMsg = "You must be a judge to perform this action.";
-export const notMentorMsg = "You must be a mentor to perform this action.";
 
 export const genericErrMsg = "An error occurred. Please try again.";
